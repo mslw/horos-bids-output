d93cba8648188652bc7826b8fb216375b8885d51
Michał Szczepanik <m.szczepanik@nencki.gov.pl>
2017-08-29 14:58:48 -0700

    Init with a modified template

---
+++ BidsOutputFilter.m
@@ -0,0 +1,22 @@
+//
+//  PluginTemplateFilter.m
+//  PluginTemplate
+//
+//  Copyright (c) CURRENT_YEAR YOUR_NAME. All rights reserved.
+//
+
+#import "BidsOutputFilter.h"
+
+@implementation BidsOutputFilter
+
+- (void) initPlugin
+{
+}
+
+- (long) filterImage:(NSString*) menuName
+{
+
+    return 0;
+}
+
+@end
