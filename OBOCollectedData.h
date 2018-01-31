f6b0a1217776cbe240fccca7427854be252931e4
Michał Szczepanik <m.szczepanik@nencki.gov.pl>
2018-01-31 17:47:56 +0100

    Put missing license notices in some files

--- OBOCollectedData.h
+++ OBOCollectedData.h
@@ -2,9 +2,22 @@
 //  OBOCollectedData.h
 //  OsirixBidsOutput
 //
-//  Created by Michał Szczepanik on 31.08.2017.
+//  Copyright (c) 2018 Michał Szczepanik.
 //
+//  This file is part of Osirix / Horos BIDS Output Extension.
 //
+//  BIDS Output Extension is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  BIDS Output Extension is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with BIDS Output Extension. If not, see <http://www.gnu.org/licenses/>.
 
 #import <Foundation/Foundation.h>
 
